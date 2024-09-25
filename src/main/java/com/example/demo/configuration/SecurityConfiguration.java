4acebe4241394d9b10f4f82a6c28bb94de62cca1
WellingtonSamuel <145589992+WellingtonSamuel@users.noreply.github.com>
2024-09-25 20:37:17 -0300

    as

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().enable();
+    http.csrf().disable();
     http.headers().disable();
     http.httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint(){
 
